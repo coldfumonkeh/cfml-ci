467f02e05dab34db3367fd83d34ecd1cddeb8cb0
Lampei@gmail.com <Lampei@gmail.com>
2014-01-10 00:22:34 -0500

    removed extraneous variables, added check for build_dir to match basedir if nothing set initially for batch files

--- tests/ci/scripts/ci-helper-railo.bat
+++ tests/ci/scripts/ci-helper-railo.bat
@@ -7,6 +7,11 @@ call %curpath%ci-helper-base.bat %1 %2
 if "%1"=="install" (
   move %WORK_DIR%\railo-express* %WORK_DIR%\railo
   move %WORK_DIR%\mxunit* %WORK_DIR%\railo\webapps\www\mxunit
+  
+  REM if nothing passed in for build directory, use scripts directory
+  if "%BUILD_DIR%"=="" (
+    set BUILD_DIR=%BASE_DIR%
+  )
 
   echo creating symlink to scripts directory %BUILD_DIR% railo\webapps\www\%2
   mklink /D %WORK_DIR%\railo\webapps\www\%2 %BUILD_DIR%
